9a0e4949e2be6dc084e3648622390054e2ac553b
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-17 21:13:32 +1100

    Create js_73-82.js

---
+++ js_73-82.js
@@ -0,0 +1,10 @@
+* Golf Code
+* Selecting from Many Options with Switch Statements
+* Adding a Default Option in Switch Statements
+* Multiple Identical Options in Switch Statements
+* Replacing If Else Chains with Switch
+* Returning Boolean Values from Functions
+* Return Early Pattern for Functions
+* Counting Cards
+* Build JavaScript Objects
+* Accessing Object Properties with Dot Notation
